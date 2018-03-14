94da289d480b9e7de7445a31c71f3c8dc96a3098
HurYur <91nttt91>
2018-03-14 12:28:11 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Link } from "react-router-dom";
 
 import logo from './logo.svg';
 import './App.css';
@@ -7,17 +8,36 @@ import PostList from "./components/PostsList";
 
 class App extends Component {
   render() {
+      const Home = () => (
+          <div>
+              <h2>Home</h2>
+          </div>
+      );
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">DevaTwitt to React</h1>
+          <nav className="menu">
+              <ul>
+                  <li>
+                      <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                      <Link to="/users">UserList</Link>
+                  </li>
+                  <li>
+                      <Link to="/posts">PostList</Link>
+                  </li>
+              </ul>
+          </nav>
         </header>
-        <p className="App-intro">
-          Let's build small Twitter
-        </p>
-        <UserList/>
-        <PostList/>
+        <main>
+            <Route exact path="/" component={Home} />
+            <Route path="/users" component={UserList} />
+            <Route path="/posts" component={PostList} />
+        </main>
       </div>
     );
   }
