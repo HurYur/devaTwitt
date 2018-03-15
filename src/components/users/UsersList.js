3b17c04036fca3d69d801b6b3f054082e4934b84
Yura Hurnyak <yuriyhurnyak@gmail.com>
2018-03-15 10:30:30 +0200

    add filtering active users

--- src/components/users/UsersList.js
+++ src/components/users/UsersList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 import loadData from '../../api/fetchData';
 
 class UsersList extends React.Component {
     constructor(props){
         super(props);
-        this.state = {users: []};
+        this.state = {users: [], showActive: false};
     }
     componentDidMount() {
         loadData("../api/users.json", {'method': 'get'})
@@ -14,13 +15,22 @@ class UsersList extends React.Component {
             });
     }
     render(){
+        let {users, showActive} = this.state;
 
         return (
             <ul>
+                <Button bsStyle="info" onClick={()=> this.setState({showActive: !showActive})}>
+                    {showActive ? "Show all users" : "Show active users"}
+                </Button>
                 {
-                    this.state.users.map((user, i)=>{
-                        return <li key={i}>{user.email}</li>
-                    })
+                    //@todo make refactoring
+                    showActive
+                        ? users.filter((user)=> user.isActive).map((user, i) =>{
+                            return <li key={i}>{user.email}</li>
+                        })
+                        : users.map((user, i)=>{
+                            return <li key={i}>{user.email}</li>
+                        })
                 }
             </ul>
         )
