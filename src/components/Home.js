aa086e660130399a411174606036371dc296da15
HurYur <91nttt91>
2018-03-16 17:51:17 +0200

    add warning when no followed posts found

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row, Col} from 'react-bootstrap'
+import {Row, Col, Alert} from 'react-bootstrap';
 
 import Post from './posts/Post';
 import {followedPosts} from "../helpers/requstHelper";
@@ -18,6 +18,11 @@ class Home extends React.Component{
             <Row>
                 <Col sm={8} smOffset={2}>
                     <h2>Home</h2>
+                    {
+                        this.state.posts.length < 1 && <Alert bsStyle="warning">
+                            You don't follow any user<br/> or <br/>Users you are following haven't publications
+                        </Alert>
+                    }
                     <div>
                         {
                             this.state.posts.sort((a,b)=> new Date(b.date) - new Date(a.date)).map((post, i)=>{
